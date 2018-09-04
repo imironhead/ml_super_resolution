6ba18c09e2b4c3cfb9bdb500147224ce8697671f
ironhead <ironhead.chuang@gmail.com>
2018-09-05 00:59:28 +0800

    - save extracted patches to tfrecord. - add methods to load patches as tensorflow dataset

--- espcn/espcn/dataset.py
+++ espcn/espcn/dataset.py
@@ -1,11 +1,73 @@
 """
 """
+import itertools
 import os
 
 import numpy as np
 import skimage.filters
 import skimage.io
 import tensorflow as tf
+
+
+def decode_patch_pair(patch_pair_record):
+    """
+    decode lr_patch & hr_patch from one TFRecord
+    """
+    features = tf.parse_single_example(
+        patch_pair_record,
+        features={
+            'lr_pixels': tf.FixedLenFeature([], tf.string),
+            'lr_height': tf.FixedLenFeature([], tf.int64),
+            'lr_width': tf.FixedLenFeature([], tf.int64),
+            'lr_depth': tf.FixedLenFeature([], tf.int64),
+            'hr_pixels': tf.FixedLenFeature([], tf.string),
+            'hr_height': tf.FixedLenFeature([], tf.int64),
+            'hr_width': tf.FixedLenFeature([], tf.int64),
+            'hr_depth': tf.FixedLenFeature([], tf.int64),
+        })
+
+    lrh = tf.cast(features['lr_height'], tf.int32)
+    lrw = tf.cast(features['lr_width'], tf.int32)
+    lrd = tf.cast(features['lr_depth'], tf.int32)
+
+    hrh = tf.cast(features['hr_height'], tf.int32)
+    hrw = tf.cast(features['hr_width'], tf.int32)
+    hrd = tf.cast(features['hr_depth'], tf.int32)
+
+    lr_patch = tf.decode_raw(features['lr_pixels'], tf.float32)
+    hr_patch = tf.decode_raw(features['hr_pixels'], tf.float32)
+
+    lr_patch = tf.reshape(lr_patch, [lrh, lrw, lrd])
+    hr_patch = tf.reshape(hr_patch, [hrh, hrw, hrd])
+
+    return lr_patch, hr_patch
+
+
+def build_image_batch_iterator(dir_path, batch_size=32):
+    """
+    read TFRecord batch. each record contains one lr_patch & one hr_patch.
+    """
+    # NOTE: build path list dataset, the '*' is must for google cloud storage
+    path_pattern = os.path.join(dir_path, '*.tfrecord')
+
+    data = tf.data.Dataset.list_files(path_pattern, shuffle=True)
+
+    # NOTE: the path generator never ends
+    data = data.repeat()
+
+    # NOTE: read tfrecord
+    data = tf.data.TFRecordDataset(data, num_parallel_reads=16)
+
+    # NOTE: decode tfrecord to get lr_patch and hr_patch
+    data = data.map(decode_patch_pair)
+
+    # NOTE: combine images to batch
+    data = data.batch(batch_size=batch_size)
+
+    # NOTE: create the final iterator
+    iterator = data.make_initializable_iterator()
+
+    return iterator
 
 
 def extract_image_patches(hr_image_path, upscaling_factor, hr_patch_size):
@@ -37,53 +99,96 @@ def extract_image_patches(hr_image_path, upscaling_factor, hr_patch_size):
     lr_patch_size = hr_patch_size // upscaling_factor
 
     # generator
-    for y in range(0, h - hr_patch_size, hr_patch_size):
-        for x in range(0, w - hr_patch_size, hr_patch_size):
-            hr_patch = hr_image[y:y+hr_patch_size, x:x+hr_patch_size]
+    range_y = range(0, h - hr_patch_size, hr_patch_size)
+    range_x = range(0, w - hr_patch_size, hr_patch_size)
 
-            lr_patch = bl_image[
-                y+offset:y+offset+hr_patch_size:upscaling_factor,
-                x+offset:x+offset+hr_patch_size:upscaling_factor]
+    for x, y in itertools.product(range_x, range_y):
+        hr_patch = hr_image[y:y+hr_patch_size, x:x+hr_patch_size]
 
-            # NOTE: reshape for espcn training
-            # NOTE: assume hr_patch is (mono channel)
-            #       [
-            #           [[1.1], [1.2], [2.1], [2.2]],
-            #           [[1.3], [1.4], [2.3], [2.4]],
-            #           [[3.1], [3.2], [4.1], [4.2]],
-            #           [[3.3], [3.4], [4.3], [4.4]],
-            #       ]
-            #
-            #       split to 2 parts along axis 1 (horizontally)
-            #       [                   |   [
-            #           [[1.1], [1.2]], |       [[2.1], [2.2]],
-            #           [[1.3], [1.4]], |       [[2.3], [2.4]],
-            #           [[3.1], [3.2]], |       [[4.1], [4.2]],
-            #           [[3.3], [3.4]], |       [[4.3], [4.4]],
-            #       ]                   |   ]
-            hr_sub_patches = np.split(hr_patch, lr_patch_size, axis=1)
+        lr_patch = bl_image[
+            y+offset:y+offset+hr_patch_size:upscaling_factor,
+            x+offset:x+offset+hr_patch_size:upscaling_factor]
 
-            # NOTE: reshape each sub patch:
-            #       [                           |   [
-            #           [[1.1, 1.2, 1.3, 1.4]], |       [[2.1, 2.2, 2.3, 2.4]],
-            #           [[3.1, 3.2, 3.3, 3.4]], |       [[4.1, 4.2, 4.3, 4.4]],
-            #       ]                           |   ]
-            new_shape = [lr_patch_size, 1, -1]
+        # NOTE: reshape for espcn training
+        # NOTE: assume hr_patch is (mono channel)
+        #       [
+        #           [[1.1], [1.2], [2.1], [2.2]],
+        #           [[1.3], [1.4], [2.3], [2.4]],
+        #           [[3.1], [3.2], [4.1], [4.2]],
+        #           [[3.3], [3.4], [4.3], [4.4]],
+        #       ]
+        #
+        #       split to 2 parts along axis 1 (horizontally)
+        #       [                   |   [
+        #           [[1.1], [1.2]], |       [[2.1], [2.2]],
+        #           [[1.3], [1.4]], |       [[2.3], [2.4]],
+        #           [[3.1], [3.2]], |       [[4.1], [4.2]],
+        #           [[3.3], [3.4]], |       [[4.3], [4.4]],
+        #       ]                   |   ]
+        hr_sub_patches = np.split(hr_patch, lr_patch_size, axis=1)
 
-            hr_sub_patches = [hsp.reshape(new_shape) for hsp in hr_sub_patches]
+        # NOTE: reshape each sub patch:
+        #       [                           |   [
+        #           [[1.1, 1.2, 1.3, 1.4]], |       [[2.1, 2.2, 2.3, 2.4]],
+        #           [[3.1, 3.2, 3.3, 3.4]], |       [[4.1, 4.2, 4.3, 4.4]],
+        #       ]                           |   ]
+        new_shape = [lr_patch_size, 1, -1]
 
-            # NOTE: concat to make final label for expcn
-            #       [
-            #           [[1.1, 1.2, 1.3, 1.4], [2.1, 2.2, 2.3, 2.4]],
-            #           [[3.1, 3.2, 3.3, 3.4], [4.1, 4.2, 4.3, 4.4]],
-            #       ]
-            hr_patch = np.concatenate(hr_sub_patches, axis=1)
+        hr_sub_patches = [hsp.reshape(new_shape) for hsp in hr_sub_patches]
 
-            yield hr_patch, lr_patch
+        # NOTE: concat to make final label for expcn
+        #       [
+        #           [[1.1, 1.2, 1.3, 1.4], [2.1, 2.2, 2.3, 2.4]],
+        #           [[3.1, 3.2, 3.3, 3.4], [4.1, 4.2, 4.3, 4.4]],
+        #       ]
+        hr_patch = np.concatenate(hr_sub_patches, axis=1)
+
+        # NOTE: generate horizontally & vertically flipped versions
+        for u, v in itertools.product([-1, 1], repeat=2):
+            yield lr_patch[::u, ::v], hr_patch[::u, ::v]
+
+
+def int64_feature(v):
+    """
+    create a feature which contains a 64-bits integer
+    """
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[v]))
+
+
+def image_feature(image):
+    """
+    create a feature which contains 32-bits floats in binary format.
+    """
+    image = image.astype(np.float32).tostring()
+
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[image]))
+
+
+def write_patch(patch_path, lr_patch, hr_patch):
+    """
+    write associated patches into a TFRecord file
+    """
+    with tf.python_io.TFRecordWriter(patch_path) as writer:
+        feature = {
+            'lr_pixels': image_feature(lr_patch),
+            'lr_height': int64_feature(lr_patch.shape[0]),
+            'lr_width': int64_feature(lr_patch.shape[1]),
+            'lr_depth': int64_feature(lr_patch.shape[2]),
+            'hr_pixels': image_feature(hr_patch),
+            'hr_height': int64_feature(hr_patch.shape[0]),
+            'hr_width': int64_feature(hr_patch.shape[1]),
+            'hr_depth': int64_feature(hr_patch.shape[2]),
+        }
+
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+        writer.write(example.SerializeToString())
 
 
 def main(_):
     """
+    extract patches from all images in a specified directory. all patches are
+    made with 2 versions (origin & low-resolution) and saved to a TFRecord.
     """
     FLAGS = tf.app.flags.FLAGS
 
@@ -100,20 +205,11 @@ def main(_):
         image_name, _ = os.path.splitext(image_path)
 
         # NOTE: extract image patches and save them
-        for i, (hr_patch, lr_patch) in enumerate(patches):
-            hr_patch_name = image_name + '_{:0>4}_hr.npy'.format(i)
-            lr_patch_name = image_name + '_{:0>4}_lr.npy'.format(i)
+        for i, (lr_patch, hr_patch) in enumerate(patches):
+            patch_name = image_name + '_{:0>4}.tfrecord'.format(i)
+            patch_path = os.path.join(FLAGS.result_dir_path, patch_name)
 
-            hr_patch_path = os.path.join(FLAGS.result_dir_path, hr_patch_name)
-            lr_patch_path = os.path.join(FLAGS.result_dir_path, lr_patch_name)
-
-            # NOTE: do not know how to write to a npz file (zip inside).
-            #       eather 'wb' or 'w+b' has permission problems
-            with tf.gfile.GFile(hr_patch_path, 'wb') as hr_patch_file:
-                np.save(hr_patch_file, hr_patch)
-
-            with tf.gfile.GFile(lr_patch_path, 'wb') as lr_patch_file:
-                np.save(lr_patch_file, lr_patch)
+            write_patch(patch_path, lr_patch, hr_patch)
 
 
 if __name__ == '__main__':
